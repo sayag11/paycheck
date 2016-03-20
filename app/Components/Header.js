82fa9e882faebe4f03bcd8ef65ed2a44c1db3ba6
Yehonatan Sayag <ysayag@infinidat.com>
2016-03-20 11:21:08 +0200

    first commit

---
+++ app/Components/Header.js
@@ -0,0 +1,15 @@
+var React = require('react');
+
+module.exports = React.createClass({
+  displayName: 'Header',
+
+  render: function () {
+    return (
+    	<div className='header'>
+    	Clac Hours
+    	</div>
+		)
+	}
+});
+
+
